4fcc27882a02aa6fbe9655f7140bf051754e21a8
Martin Bjeld <mbj@Martins-MacBook-Pro.local>
2018-11-07 15:04:23 +0100

    Updates

--- src/model/RowAction.js
+++
@@ -1,7 +0,0 @@
-import { types } from "mobx-state-tree";
-
-export const RowAction = types.model("RowAction", {
-  type: types.enumeration("RowActionEnum", ["delete", "goto", "edit", "copy"]),
-  tooltip: types.maybeNull(types.string),
-  rules: types.optional(types.map(types.frozen), {})
-});
